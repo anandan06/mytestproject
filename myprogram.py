7afd3e51f3ee95cc5799a4ae33c264b3a29c4d27
Iam_Anandan <n.anandan.30@gmail.com>
2024-03-07 17:11:52 +0530

    second commit

--- myprogram.py
+++ myprogram.py
@@ -1,3 +1,11 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def helloworld():
+	if(request.method == 'GET'):
+		data = {"data": "Hello World"}
+		return jsonify(data)
+
+
